e72a31af79bca0aeebd9b0b5fba17a2d99c94dca
PabloVelascoCrespo <72821847+PabloVelascoCrespo@users.noreply.github.com>
2023-11-16 13:26:26 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import datetime
 import requests
 import json
 import requests
+
+url_clientes = "http://194.233.162.198/contadores"
+url_tipo_clientes = "http://194.233.162.198/tipo_cliente" 
+
 st.set_page_config(layout="wide")
 
 st.markdown(
@@ -42,10 +46,18 @@ def disable_selectbox(letra):
         st.session_state.cliente_disabled = True
 
 def buscar():
-    respuesta = requests.post(url_alerta_consumo_energetico, headers={'accept': 'application/json', 'Content-Type': 'application/json'}, data='{"fecha": "'+str(fecha)+'"}')
+    if  st.session_state.cliente_disabled:
+        url_alerta_consumo_energetico = 'http://194.233.162.198/early_warning?dia='+str(fecha)+'&tipo_cliente='+str(tipo_cliente)
+    else:
+        url_alerta_consumo_energetico = 'http://194.233.162.198/early_warning?dia='+str(fecha)+'&cnt='+str(cliente)
+    respuesta = requests.get(url_alerta_consumo_energetico, headers={'accept': 'application/json'})
     if( respuesta.status_code == requests.codes.ok ):
         diccionario_respuesta = json.loads(respuesta.text)
         st.subheader("Fecha: " + str(diccionario_respuesta["fecha"]))
+        if  st.session_state.tipo_cliente_disabled:
+            st.subheader("Cliente: " + str(diccionario_respuesta["cnt"]))
+        st.subheader("Tipo del Cliente: " + str(diccionario_respuesta["tipo_cliente"]))
+        st.subheader("Afectación de Consumo: Nivel = " + str(diccionario_respuesta["afectacion_de_consumo"]["nivel"]) + " y Sentido = " + str(diccionario_respuesta["afectacion_de_consumo"]["sentido"]))
         color = ""
         if str(diccionario_respuesta["tipo_de_dia_de_consumo"]["etiqueta"]) == "Muy bajo" or str(diccionario_respuesta["tipo_de_dia_de_consumo"]["etiqueta"]) == "Bajo":
             color = "green"
@@ -60,29 +72,29 @@ def buscar():
 
         col1, col2, col3, col4, col5 = st.columns(5)
 
-        col1.metric("Temperatura mínima", str(diccionario_respuesta["detalles"]["meteorologia"]["temperatura_minima"]["etiqueta"]), str(diccionario_respuesta["detalles"]["meteorologia"]["temperatura_minima"]["valor_numerico"]) + " ºC", delta_color="off")
-        col2.metric("Temperatura media", str(diccionario_respuesta["detalles"]["meteorologia"]["temperatura media"]["etiqueta"]), str(diccionario_respuesta["detalles"]["meteorologia"]["temperatura media"]["valor_numerico"]) + " ºC", delta_color="off")
-        col3.metric("Temperatura máxima", str(diccionario_respuesta["detalles"]["meteorologia"]["temperatura_maxima"]["etiqueta"]), str(diccionario_respuesta["detalles"]["meteorologia"]["temperatura_maxima"]["valor_numerico"]) + " ºC", delta_color="off")
-        col4.metric("Precipitacion", str(diccionario_respuesta["detalles"]["meteorologia"]["precipitacion"]["etiqueta"]), str(diccionario_respuesta["detalles"]["meteorologia"]["precipitacion"]["valor_numerico"]), delta_color="off")
-        col5.metric("Horas de sol", str(diccionario_respuesta["detalles"]["meteorologia"]["horas_de_sol"]["etiqueta"]), str(diccionario_respuesta["detalles"]["meteorologia"]["horas_de_sol"]["valor_numerico"]), delta_color="off")
+        col1.metric("Temperatura mínima", str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura_minima"]["etiqueta"]), str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura_minima"]["valor_numerico"]) + " ºC", delta_color="off")
+        col2.metric("Temperatura media", str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura media"]["etiqueta"]), str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura media"]["valor_numerico"]) + " ºC", delta_color="off")
+        col3.metric("Temperatura máxima", str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura_maxima"]["etiqueta"]), str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura_maxima"]["valor_numerico"]) + " ºC", delta_color="off")
+        col4.metric("Precipitacion", str(diccionario_respuesta["detalles_dia"]["meteorologia"]["precipitacion"]["etiqueta"]), str(diccionario_respuesta["detalles_dia"]["meteorologia"]["precipitacion"]["valor_numerico"]), delta_color="off")
+        col5.metric("Horas de sol", str(diccionario_respuesta["detalles_dia"]["meteorologia"]["horas_de_sol"]["etiqueta"]), str(diccionario_respuesta["detalles_dia"]["meteorologia"]["horas_de_sol"]["valor_numerico"]), delta_color="off")
 
-        if diccionario_respuesta["detalles"]["fin de semana"]:
+        if diccionario_respuesta["detalles_dia"]["fin de semana"]:
             col1.metric("Fin de semana", "Si")
         else:
             col1.metric("Fin de semana", "No")
-        col2.metric("Estación", str(diccionario_respuesta["detalles"]["estacion"]))
-        if diccionario_respuesta["detalles"]["eventos"] == []:
+        col2.metric("Estación", str(diccionario_respuesta["detalles_dia"]["estacion"]))
+        if diccionario_respuesta["detalles_dia"]["eventos"] == []:
             col3.metric("Eventos", "No")
         else:
-            for i in diccionario_respuesta["detalles"]["eventos"]:
+            for i in diccionario_respuesta["detalles_dia"]["eventos"]:
                 col3.metric("Eventos", str(i["Nombre"]), str(i["Clase"]), delta_color="off")
-        if diccionario_respuesta["detalles"]["incidencias"] == []:
+        if diccionario_respuesta["detalles_dia"]["incidencias"] == []:
             col4.metric("Incidencias", "No")
         else:
-            for i in diccionario_respuesta["detalles"]["incidencias"]:
+            for i in diccionario_respuesta["detalles_dia"]["incidencias"]:
                 col4.metric("Incidencias", i)
-        if diccionario_respuesta["detalles"]["festivo"]:
-            col5.metric("Fiesta", str(diccionario_respuesta["detalles"]["fiesta"]))
+        if diccionario_respuesta["detalles_dia"]["festivo"]:
+            col5.metric("Fiesta", str(diccionario_respuesta["detalles_dia"]["fiesta"]))
         else:
             col5.metric("Fiesta", "No")
         respuesta1 = requests.post(url_alerta_consumo_energetico, headers={'accept': 'application/json', 'Content-Type': 'application/json'}, data='{"fecha": "'+str(fecha+timedelta(days=-1))+'"}')
@@ -101,10 +113,6 @@ def buscar():
 def reiniciar():
     st.session_state.cliente_disabled = False
     st.session_state.tipo_cliente_disabled = False
-
-url_clientes = "http://194.233.162.198/contadores"
-url_tipo_clientes = "http://194.233.162.198/tipo_cliente" 
-url_alerta_consumo_energetico = 'http://194.233.162.198/alerta_consumo_energetico'
 
 json_clientes = requests.get(url_clientes).text
 json_tipos_cliente = requests.get(url_tipo_clientes).text
