9969972adc52a4f715c937fc786dcb8661d175e2
PabloVelascoCrespo <72821847+PabloVelascoCrespo@users.noreply.github.com>
2023-11-21 13:35:08 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -11,24 +11,13 @@ url_tipo_clientes = "http://194.233.162.198/tipo_cliente"
 fecha = ""
 tipo_cliente=""
 cliente=""
+json_clientes = requests.get(url_clientes).text
+json_tipos_cliente = requests.get(url_tipo_clientes).text
+
+lista_clientes = json.loads(json_clientes)["contadores"]
+lista_tipos_cliente = json.loads(json_tipos_cliente)["tipo_cliente"]
 
 st.set_page_config(layout="wide")
-
-st.markdown(
-    """
-<style>
-[data-testid="stMetricValue"] {
-    font-size: 18px;
-}
-</style>
-<style>
-[data-testid="stMetricDelta"] svg {
-    display: none;
-}
-</style>
-""",
-    unsafe_allow_html=True,
-)
 
 if "cliente_disabled" not in st.session_state:
     st.session_state.cliente_disabled = False
@@ -36,6 +25,12 @@ if "cliente_disabled" not in st.session_state:
     st.session_state.fecha_disabled = False
     st.session_state.disable_all = False
 
+def reiniciar():
+    st.session_state["current_page"] = "main_page"
+    st.session_state.cliente_disabled = False
+    st.session_state.tipo_cliente_disabled = False
+    st.session_state.cnt_selectbox = '-------------------------'
+    st.session_state.tipo_cliente_selectbox = '-------------------------'
 
 def ColourWidgetText(wgt_txt, wch_colour = '#000000'):
     htmlstr = """<script>var elements = window.parent.document.querySelectorAll('*'), i;
@@ -54,158 +49,175 @@ def disable_selectbox(letra):
 def disable_all():
     st.session_state.disable_all = True
 
-def buscar():
-    disable_all()
-    if  st.session_state.cliente_disabled:
-        url_alerta_consumo_energetico = 'http://194.233.162.198/early_warning?dia='+str(fecha)+'&tipo_cliente='+str(tipo_cliente)
-        st.title("Búsqueda por tipo de cliente")
+def data_page():
+    url_alerta_consumo_energetico = ""
+    respuesta = ""
+    if st.session_state["cnt"] == '-------------------------' and st.session_state["tipo_cliente"] == '-------------------------':
+        st.title("Debes seleccionar un cliente o un tipo de cliente.")
+    elif st.session_state["cnt"] == '-------------------------':
+        url_alerta_consumo_energetico = 'http://194.233.162.198/early_warning?dia='+str(st.session_state["fecha"])+'&tipo_cliente='+str(st.session_state["tipo_cliente"])
     else:
-        url_alerta_consumo_energetico = 'http://194.233.162.198/early_warning?dia='+str(fecha)+'&cnt='+str(cliente)
-        st.title("Búsqueda por cliente")
-    respuesta = requests.get(url_alerta_consumo_energetico, headers={'accept': 'application/json'})
-    print(url_alerta_consumo_energetico)
-    if( respuesta.status_code == requests.codes.ok ):
-        diccionario_respuesta = json.loads(respuesta.text)
-        st.subheader("Fecha:")
-        st.write(str(diccionario_respuesta["fecha"]))
-        if  not st.session_state.cliente_disabled:
-            st.subheader("Cliente:")
-            st.write(str(diccionario_respuesta["cnt"]))
-        st.subheader("Tipo del Cliente:")
-        st.write(str(diccionario_respuesta["tipo_cliente"]))
-
-        st.subheader("Afectación de Consumo:")
-        col1,col2,col3,col4,col5 = st.columns(5)
-
-        flecha = ""
-
-        col1.metric("Nivel", str(diccionario_respuesta["afectacion_de_consumo"]["nivel"]))
-
-        if str(diccionario_respuesta["afectacion_de_consumo"]["sentido"]) == "Alza":
-            flecha = "⬆"
-        elif str(diccionario_respuesta["afectacion_de_consumo"]["sentido"]) == "Baja":
-            flecha = "⬇"
-        else:
-            flecha = "→"
-
-        col2.metric("Sentido", flecha+str(diccionario_respuesta["afectacion_de_consumo"]["sentido"]))
-
-        color = ""
-        if str(diccionario_respuesta["tipo_de_dia_de_consumo"]["etiqueta"]) == "Muy bajo" or str(diccionario_respuesta["tipo_de_dia_de_consumo"]["etiqueta"]) == "Bajo":
-            color = "green"
-        elif str(diccionario_respuesta["tipo_de_dia_de_consumo"]["etiqueta"]) == "Muy alto" or str(diccionario_respuesta["tipo_de_dia_de_consumo"]["etiqueta"]) == "Alto":
-            color = "red"
-        elif str(diccionario_respuesta["tipo_de_dia_de_consumo"]["etiqueta"]) == "Regular":
-            color = "blue"
-        else:
-            color = "gray"
-        st.subheader("Consumo del día:")
-        st.write(":"+color+"[" + str(diccionario_respuesta["tipo_de_dia_de_consumo"]["etiqueta"]) + "]")
-
-        st.subheader("Meteorología")
-
-        col1, col2, col3, col4, col5 = st.columns(5)
-
-        col1.metric("Temperatura mínima", str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura_minima"]["etiqueta"]), str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura_minima"]["valor_numerico"]) + " ºC", delta_color="off")
-        col2.metric("Temperatura media", str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura media"]["etiqueta"]), str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura media"]["valor_numerico"]) + " ºC", delta_color="off")
-        col3.metric("Temperatura máxima", str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura_maxima"]["etiqueta"]), str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura_maxima"]["valor_numerico"]) + " ºC", delta_color="off")
-        col4.metric("Precipitacion", str(diccionario_respuesta["detalles_dia"]["meteorologia"]["precipitacion"]["etiqueta"]), str(diccionario_respuesta["detalles_dia"]["meteorologia"]["precipitacion"]["valor_numerico"]), delta_color="off")
-        col5.metric("Horas de sol", str(diccionario_respuesta["detalles_dia"]["meteorologia"]["horas_de_sol"]["etiqueta"]), str(diccionario_respuesta["detalles_dia"]["meteorologia"]["horas_de_sol"]["valor_numerico"]), delta_color="off")
-
-        if diccionario_respuesta["detalles_dia"]["fin de semana"]:
-            col1.metric("Fin de semana", "Si")
-        else:
-            col1.metric("Fin de semana", "No")
-        col2.metric("Estación", str(diccionario_respuesta["detalles_dia"]["estacion"]))
-        if diccionario_respuesta["detalles_dia"]["eventos"] == []:
-            col3.metric("Eventos", "No")
-        else:
-            for i in diccionario_respuesta["detalles_dia"]["eventos"]:
-                col3.metric("Eventos", str(i["Nombre"]), str(i["Clase"]), delta_color="off")
-        if diccionario_respuesta["detalles_dia"]["incidencias"] == []:
-            col4.metric("Incidencias", "No")
-        else:
-            for i in diccionario_respuesta["detalles_dia"]["incidencias"]:
-                col4.metric("Incidencias", i)
-        if diccionario_respuesta["detalles_dia"]["festivo"]:
-            col5.metric("Fiesta", str(diccionario_respuesta["detalles_dia"]["fiesta"]))
-        else:
-            col5.metric("Fiesta", "No")
-        respuesta1 = requests.post(url_alerta_consumo_energetico, headers={'accept': 'application/json', 'Content-Type': 'application/json'}, data='{"fecha": "'+str(fecha+timedelta(days=-1))+'"}')
-        respuesta2 = requests.post(url_alerta_consumo_energetico, headers={'accept': 'application/json', 'Content-Type': 'application/json'}, data='{"fecha": "'+str(fecha+timedelta(days=-2))+'"}')
-        if( respuesta1.status_code == requests.codes.ok ):
-            diccionario_respuesta1 = json.loads(respuesta1.text)
-            col4.metric("Día "+str(fecha+timedelta(days=-1))+":", "Consumo:", str(diccionario_respuesta1["tipo_de_dia_de_consumo"]["etiqueta"]), delta_color="off")
-        if( respuesta2.status_code == requests.codes.ok ):
-            diccionario_respuesta2 = json.loads(respuesta2.text)
-            col2.metric("Día "+str(fecha+timedelta(days=-2))+":", "Consumo:", str(diccionario_respuesta2["tipo_de_dia_de_consumo"]["etiqueta"]), delta_color="off")
-
+        url_alerta_consumo_energetico = 'http://194.233.162.198/early_warning?dia='+str(st.session_state["fecha"])+'&cnt='+str(st.session_state["cnt"])
+    if url_alerta_consumo_energetico == "":
+        pass
     else:
-        st.write("Día no encontrado")
+        respuesta = requests.get(url_alerta_consumo_energetico, headers={'accept': 'application/json'})
+        if( respuesta.status_code == requests.codes.ok ):
+            if st.session_state["cnt"] == '-------------------------':
+                st.title("Búsqueda por tipo de cliente")
+            else:
+                st.title("Búsqueda por cliente")
+            diccionario_respuesta = json.loads(respuesta.text)
+            st.subheader("Fecha:")
+            st.write( diccionario_respuesta["fecha"][8:10]+"/"+diccionario_respuesta["fecha"][5:7]+"/"+diccionario_respuesta["fecha"][0:4])
+            if st.session_state["tipo_cliente"] == '-------------------------':
+                st.subheader("Cliente:")
+                st.write(str(diccionario_respuesta["cnt"]))
+            st.subheader("Tipo del Cliente:")
+            st.write(str(diccionario_respuesta["tipo_cliente"]))
 
-    a,b,c = st.columns(3)
+            st.subheader("Afectación de Consumo:")
+            col1,col2,col3,col4,col5 = st.columns(5)
 
-    b.button('REINICIAR', on_click=reiniciar)
+            flecha = ""
 
-def reiniciar():
-    st.session_state.cliente_disabled = False
-    st.session_state.tipo_cliente_disabled = False
-    st.session_state.fecha_disabled = False
-    st.session_state.disable_all = False
+            col1.metric("Nivel", str(diccionario_respuesta["afectacion_de_consumo"]["nivel"]))
 
-json_clientes = requests.get(url_clientes).text
-json_tipos_cliente = requests.get(url_tipo_clientes).text
+            if str(diccionario_respuesta["afectacion_de_consumo"]["sentido"]) == "Alza":
+                flecha = "⬆"
+            elif str(diccionario_respuesta["afectacion_de_consumo"]["sentido"]) == "Baja":
+                flecha = "⬇"
+            else:
+                flecha = "→"
 
-lista_clientes = json.loads(json_clientes)["contadores"]
-lista_tipos_cliente = json.loads(json_tipos_cliente)["tipo_cliente"]
+            col2.metric("Sentido", flecha+str(diccionario_respuesta["afectacion_de_consumo"]["sentido"]))
 
-col_fecha, col_cliente, col_tipo_cliente = st.columns(3)
+            color = ""
+            if str(diccionario_respuesta["tipo_de_dia_de_consumo"]["etiqueta"]) == "Muy bajo" or str(diccionario_respuesta["tipo_de_dia_de_consumo"]["etiqueta"]) == "Bajo":
+                color = "green"
+            elif str(diccionario_respuesta["tipo_de_dia_de_consumo"]["etiqueta"]) == "Muy alto" or str(diccionario_respuesta["tipo_de_dia_de_consumo"]["etiqueta"]) == "Alto":
+                color = "red"
+            elif str(diccionario_respuesta["tipo_de_dia_de_consumo"]["etiqueta"]) == "Regular":
+                color = "blue"
+            else:
+                color = "gray"
+            st.subheader("Consumo del día:")
+            st.write(":"+color+"[" + str(diccionario_respuesta["tipo_de_dia_de_consumo"]["etiqueta"]) + "]")
 
-col_fecha, col_cliente, col_tipo_cliente = st.columns(3)
+            st.subheader("Meteorología")
 
-ColourWidgetText('Primavera', '#F586E3')
-ColourWidgetText('Verano', '#FF7B21')
-ColourWidgetText('Otoño', '#B74E0D')
-ColourWidgetText('Invierno', '#AEEDE6')
+            col1, col2, col3, col4, col5 = st.columns(5)
 
-ColourWidgetText('Muy fria', '#83FFFD')
-ColourWidgetText('Fria', '#10EFFF')
-ColourWidgetText('Templada', '#42FF35')
-ColourWidgetText('Calurosa', '#FF0000')
-ColourWidgetText('Muy calurosa', '#B30000')
+            col1.metric("Temperatura mínima", str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura_minima"]["etiqueta"]), str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura_minima"]["valor_numerico"]) + " ºC", delta_color="off")
+            col2.metric("Temperatura media", str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura media"]["etiqueta"]), str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura media"]["valor_numerico"]) + " ºC", delta_color="off")
+            col3.metric("Temperatura máxima", str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura_maxima"]["etiqueta"]), str(diccionario_respuesta["detalles_dia"]["meteorologia"]["temperatura_maxima"]["valor_numerico"]) + " ºC", delta_color="off")
+            col4.metric("Precipitacion", str(diccionario_respuesta["detalles_dia"]["meteorologia"]["precipitacion"]["etiqueta"]), str(diccionario_respuesta["detalles_dia"]["meteorologia"]["precipitacion"]["valor_numerico"]), delta_color="off")
+            col5.metric("Horas de sol", str(diccionario_respuesta["detalles_dia"]["meteorologia"]["horas_de_sol"]["etiqueta"]), str(diccionario_respuesta["detalles_dia"]["meteorologia"]["horas_de_sol"]["valor_numerico"]), delta_color="off")
 
-ColourWidgetText('Bajo', 'green')
-ColourWidgetText('Muy bajo', 'green')
-ColourWidgetText('Regular', 'blue')
-ColourWidgetText('Alto', 'red')
-ColourWidgetText('Muy alto', 'red')
+            if diccionario_respuesta["detalles_dia"]["fin de semana"]:
+                col1.metric("Fin de semana", "Si")
+            else:
+                col1.metric("Fin de semana", "No")
+            col2.metric("Estación", str(diccionario_respuesta["detalles_dia"]["estacion"]))
+            if diccionario_respuesta["detalles_dia"]["eventos"] == []:
+                col3.metric("Eventos", "No")
+            else:
+                for i in diccionario_respuesta["detalles_dia"]["eventos"]:
+                    col3.metric("Eventos", str(i["Nombre"]), str(i["Clase"]), delta_color="off")
+            if diccionario_respuesta["detalles_dia"]["incidencias"] == []:
+                col4.metric("Incidencias", "No")
+            else:
+                for i in diccionario_respuesta["detalles_dia"]["incidencias"]:
+                    col4.metric("Incidencias", i)
+            if diccionario_respuesta["detalles_dia"]["festivo"]:
+                col5.metric("Fiesta", str(diccionario_respuesta["detalles_dia"]["fiesta"]))
+            else:
+                col5.metric("Fiesta", "No")
 
-ColourWidgetText('Mucho', 'red')
-ColourWidgetText('Poco', 'green')
-ColourWidgetText('⬆Alza', 'red')
-ColourWidgetText('⬇Baja', 'green')
+        else:
+            st.title("Día no encontrado")
 
+    a,b,c,d,e = st.columns(5)
 
-if not st.session_state.disable_all:
+    c.button("REINICIAR", on_click=reiniciar)
+    st.markdown(
+    """
+<style>
+[data-testid="stMetricValue"] {
+    font-size: 18px;
+}
+</style>
+<style>
+[data-testid="stMetricDelta"] svg {
+    display: none;
+}
+</style>
+""",
+    unsafe_allow_html=True,
+)
+    ColourWidgetText('Primavera', '#F586E3')
+    ColourWidgetText('Verano', '#FF7B21')
+    ColourWidgetText('Otoño', '#B74E0D')
+    ColourWidgetText('Invierno', '#AEEDE6')
+
+    ColourWidgetText('Muy fria', '#83FFFD')
+    ColourWidgetText('Fria', '#10EFFF')
+    ColourWidgetText('Templada', '#42FF35')
+    ColourWidgetText('Calurosa', '#FF0000')
+    ColourWidgetText('Muy calurosa', '#B30000')
+
+    ColourWidgetText('Bajo', 'green')
+    ColourWidgetText('Muy bajo', 'green')
+    ColourWidgetText('Regular', 'blue')
+    ColourWidgetText('Alto', 'red')
+    ColourWidgetText('Muy alto', 'red')
+
+    ColourWidgetText('Mucho', 'red')
+    ColourWidgetText('Poco', 'green')
+    ColourWidgetText('No afecta', 'blue')
+    ColourWidgetText('⬆Alza', 'red')
+    ColourWidgetText('⬇Baja', 'green')
+    ColourWidgetText('→No afecta', 'blue')
+
+def main_page():
+    col_fecha, col_cliente, col_tipo_cliente = st.columns(3)
+
     with col_fecha:
-        fecha = st.date_input("Fecha", datetime.datetime.today(), disabled=st.session_state.fecha_disabled, format="DD/MM/YYYY")
+        st.session_state["fecha"] = st.date_input("Fecha", datetime.datetime.today(), disabled=st.session_state.fecha_disabled, format="DD/MM/YYYY")
     with col_cliente:
-        cliente = st.selectbox(
+        opciones_cnt = ['-------------------------']+lista_clientes
+        st.session_state["cnt"] = st.selectbox(
             'Selecciona el cliente',
-            lista_clientes,
-            disabled=st.session_state.cliente_disabled,
+            opciones_cnt,
+            key="cnt_selectbox",
             on_change=disable_selectbox,
-            args="a"
+            args="a",
+            disabled=st.session_state.cliente_disabled
         )
-        st.button('BUSCAR', on_click=buscar)
-        st.button('REINICIAR', on_click=reiniciar)
 
 
     with col_tipo_cliente:
-        tipo_cliente = st.selectbox( 
+        opciones_tipo_cliente = ['-------------------------']+lista_tipos_cliente
+        st.session_state["tipo_cliente"] = st.selectbox( 
             'Selecciona el tipo del cliente',
-            lista_tipos_cliente,
-            disabled=st.session_state.tipo_cliente_disabled,
+            opciones_tipo_cliente,
+            key="tipo_cliente_selectbox",
             on_change=disable_selectbox,
-            args="b"
+            args="b",
+            disabled=st.session_state.tipo_cliente_disabled
         )
+    if col_cliente.button("BUSCAR"):
+        st.session_state["current_page"] = "data_page"
+        st.rerun()
+
+    col_cliente.button("REINICIAR", on_click=reiniciar)
+
+def main():
+    st.session_state.setdefault("current_page", "main_page")
+    if st.session_state["current_page"] == "main_page":
+        main_page()
+    elif st.session_state["current_page"] == "data_page":
+        data_page()
+
+if __name__ == '__main__':
+    main()
